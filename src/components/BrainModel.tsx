1cdd9b16a644b19501659f4c033f4f336912c3d2
Rahul <your.email@example.com>
2025-06-23 10:01:13 +0530

    Make brain arrows point inward toward brain and reduce size for better mobile experience

--- src/components/BrainModel.tsx
+++ src/components/BrainModel.tsx
@@ -46,14 +46,14 @@ export default function BrainModel({ highlightedRegions = [] }: { highlightedReg
       <primitive object={scene} scale={0.1} position={[0, 0, 0]} />
 
       {highlightedRegions.includes('Parietal') && (
-        <mesh position={[0, 0.18, 0]} scale={[0.035, 0.035, 0.035]} rotation={[0, 0, 0]}>
+        <mesh position={[0, 0.15, 0]} scale={[0.025, 0.025, 0.025]} rotation={[Math.PI, 0, 0]}>
           <coneGeometry args={[1, 3, 6]} />
           <meshStandardMaterial color={0xffff00} emissive={0x444400} emissiveIntensity={0.8} />
         </mesh>
       )}
 
       {highlightedRegions.includes('Frontal') && (
-        <mesh position={[0, 0.02, 0.15]} scale={[0.03, 0.03, 0.03]} rotation={[Math.PI / 2, 0, 0]}>
+        <mesh position={[0, 0.02, 0.12]} scale={[0.02, 0.02, 0.02]} rotation={[-Math.PI / 2, 0, 0]}>
           <coneGeometry args={[1, 3, 6]} />
           <meshStandardMaterial color={0x0066ff} emissive={0x002244} emissiveIntensity={0.8} />
         </mesh>
@@ -61,11 +61,11 @@ export default function BrainModel({ highlightedRegions = [] }: { highlightedReg
 
       {highlightedRegions.includes('Temporal') && (
         <>
-          <mesh position={[0.15, 0, 0]} scale={[0.03, 0.03, 0.03]} rotation={[0, 0, -Math.PI / 2]}>
+          <mesh position={[0.12, 0, 0]} scale={[0.02, 0.02, 0.02]} rotation={[0, 0, Math.PI / 2]}>
             <coneGeometry args={[1, 3, 6]} />
             <meshStandardMaterial color={0x00ff66} emissive={0x004422} emissiveIntensity={0.8} />
           </mesh>
-          <mesh position={[-0.15, 0, 0]} scale={[0.03, 0.03, 0.03]} rotation={[0, 0, Math.PI / 2]}>
+          <mesh position={[-0.12, 0, 0]} scale={[0.02, 0.02, 0.02]} rotation={[0, 0, -Math.PI / 2]}>
             <coneGeometry args={[1, 3, 6]} />
             <meshStandardMaterial color={0x00ff66} emissive={0x004422} emissiveIntensity={0.8} />
           </mesh>
@@ -73,7 +73,7 @@ export default function BrainModel({ highlightedRegions = [] }: { highlightedReg
       )}
 
       {highlightedRegions.includes('Occipital') && (
-        <mesh position={[0, 0.02, -0.15]} scale={[0.03, 0.03, 0.03]} rotation={[-Math.PI / 2, 0, 0]}>
+        <mesh position={[0, 0.02, -0.12]} scale={[0.02, 0.02, 0.02]} rotation={[Math.PI / 2, 0, 0]}>
           <coneGeometry args={[1, 3, 6]} />
           <meshStandardMaterial color={0xff3300} emissive={0x441100} emissiveIntensity={0.8} />
         </mesh>
